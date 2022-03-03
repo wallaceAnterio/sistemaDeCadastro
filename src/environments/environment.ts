17df09787547473edccdc696d2ac65cf753584c2
Wallace Anterio <54643137+wallaceAnterio@users.noreply.github.com>
2022-03-03 05:01:12 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyB1S348_euZlbRDsyxq7wTSHjmJeUoDGLw',
-    authDomain: 'sistema-de-cadastro-2022.firebaseapp.com',
-    projectId: 'sistema-de-cadastro-2022',
-    storageBucket: 'sistema-de-cadastro-2022.appspot.com',
-    messagingSenderId: '172214433533',
-    appId: '1:172214433533:web:24ce1df063e4f70c7eb094',
-    measurementId: 'G-VRRKFFD1K5',
-  },
+  production: false
 };
 
 /*

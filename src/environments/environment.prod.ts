3a55e8bcddfc8a33fb9024dcb67526f3a540d705
Wallace Anterio <54643137+wallaceAnterio@users.noreply.github.com>
2022-03-03 05:00:27 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyB1S348_euZlbRDsyxq7wTSHjmJeUoDGLw',
-    authDomain: 'sistema-de-cadastro-2022.firebaseapp.com',
-    projectId: 'sistema-de-cadastro-2022',
-    storageBucket: 'sistema-de-cadastro-2022.appspot.com',
-    messagingSenderId: '172214433533',
-    appId: '1:172214433533:web:24ce1df063e4f70c7eb094',
-    measurementId: 'G-VRRKFFD1K5',
-  },
+  production: true  
 };
